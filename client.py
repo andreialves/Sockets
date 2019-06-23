01174abb88e0e45fe9ecd46ce53602c0e1718dd7
Andrei Alves <andreialvesccp@gmail.com>
2019-06-23 16:33:19 -0300

    Tentativa de envio de arquivo

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
-import socket, threading, sys
+# Tassiana Carneiro Rios de Oliveira - 85249
+# Andrei Alef de Oliveira Alves - 85280
+
+import socket, threading, sys, pickle
+from datetime import datetime
 
 HOST = "127.0.0.1"      #Endereço ip do Servidor
 PORT = 20000            #Porta do Servidor´
@@ -6,19 +10,14 @@ PORT = 20000            #Porta do Servidor´
 class Client:
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    log = []
     
     def __init__(self, usuario):
         self.udp.sendto(usuario.encode(), (HOST, PORT))
-        try:
-            msg, serv = self.udp.recvfrom(1024)
-            if (msg.decode('utf-8') != "ACK"):
-                print("Servidor indisponível no momento.")
-                sys.exit()
-        except:
-            print("Servidor não encontrado.")        
-            sys.exit()
+        
         cThread = threading.Thread(target=self.chat)
         cThread.start()
+
         while True:
             try:
                 msg, serv = self.udp.recvfrom(1024)
@@ -27,12 +26,49 @@ class Client:
             data_string = msg.decode('utf-8')
             if not msg:
                 break
-            print(data_string)
+            if (data_string == "ACK"):
+                self.log.append("Log: " + str(datetime.now()) + " - ACK")
+            elif (data_string == "LIST"):
+                self.log.append("Log: " + str(datetime.now()) + " - LIST")
+            elif (data_string ==  "BYE"):
+                self.log.append("Log: " + str(data_string.now()) + " - BYE")
+            else:
+                print(data_string)
     
     def chat(self):
         while True:
             msg = input()
             self.udp.sendto(msg.encode(), (HOST, PORT))
+
+            if msg.count('/file') == 1:
+                #Melhorar isso aqui ou só testar na abertura
+                if len(msg) < 11 and msg.count('.') != 1:
+                    print("Especifique um nome válido para o arquivo")
+                else:
+                    varqName = msg.split()
+                    print("LOG: Nome do arquivo: " + str(varqName[1]) + " Comando: " + str(varqName[0]) )
+                   
+                    try:
+                        arq = open(str(varqName[1]), 'r')
+                        info = {'nome': str(varqName[1]), 'file': arq, 'opcao': 1}
+                        print("LOG: Arquivo aberto com sucesso")
+                    except:
+                        print("Arquivo inválido, tente novamente")
+                    
+                    try:
+                        self.tcp.connect((HOST, PORT))
+                        self.tcp.sendall(pickle.dumps(info))
+                        self.tcp.close()
+                        '''
+                        for i in arq.readlines():
+                            self.tcp.send(i)
+                        '''
+                        print("LOG: Arquivo enviado com sucesso")
+                    except:
+                        print("LOG: Não enviado > Abrir conexão com o tcp")
+            if msg == "/log":
+                print (self.log)
+
             if msg == "/bye":
                 self.udp.close()
                 break
--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-import threading, socket, sys
+# Tassiana Carneiro Rios de Oliveira - 85249
+# Andrei Alef de Oliveira Alves - 85280
+
+import threading, socket, sys, pickle, time
 
 HOST = ''       #endereço do servidor
 PORT = 20000    #porta do servidor
@@ -6,23 +9,26 @@ PORT = 20000    #porta do servidor
 class Server:
 
     user = {}
-
+    info = 0
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def __init__(self):
         self.udp.bind((HOST, PORT))
+        self.tcp.bind((HOST, PORT))
+        self.tcp.listen(1)
 
     def chat(self, msg, endereco):
         data_string = msg.decode('utf-8')
         if data_string == "/bye":
-            m = "Cliente " + str(self.user[endereco]) + " desconectou"
+            m = str(self.user[endereco]) + " saiu"
             print(m)
             del self.user[endereco]
             for user in self.user.keys():
                 self.udp.sendto(m.encode(), user)
         elif data_string == "/list":
             m = "Clientes conectados: " + ", ".join(["%s" % v for v in self.user.values()])
+            self.udp.sendto("LIST".encode(), endereco)
             self.udp.sendto(m.encode(), endereco)
         else:
             m = str(self.user[endereco]) + " disse: " + data_string
@@ -30,12 +36,37 @@ class Server:
                 if(user == endereco):
                     continue
                 self.udp.sendto(m.encode(), user)
-            
-            
+
+    def envioArq(self, con, cliente):
+        while True:
+            try:
+                info_bin = b''
+                st = time.time()
+                while True:
+                    arq = con.recv(1024)
+                    if not arq:
+                        break
+                    info_bin += arq
+                    if time.time - st >= 2:
+                        print ('bytes downloaded: ', len(info_bin))
+                        st = time.time()
+                info = pickle.loads(info_bin)
+                if info['file']:
+                    dest = 'files/{}'.format(info['name'])
+                    with open(dest, 'wb') as f:
+                        f.write(info['file'])
+                    con.close()
+            except Exception as e:
+                print(e)
+                break
+
+
     def run(self):
         print("Aguardando conexões: ")
         while True:
             msg, endereco = self.udp.recvfrom(1024)
+            if msg.decode() != '':
+                a = msg.decode().split()
             if endereco not in self.user:
                 nome = msg.decode()
                 self.user[endereco] = nome
@@ -46,13 +77,14 @@ class Server:
                     if(user == endereco):
                         continue
                     self.udp.sendto(m.encode(), user)
+            elif a[0] == "/file":
+                con, cliente = self.tcp.accept()
+                serverT = threading.Thread(target=self.envioArq, args=(con, cliente))
+                serverT.start()
             else:
                 serverT = threading.Thread(target=self.chat, args=(msg, endereco))
                 serverT.start()
             
            
-            
-            
-            
 servidor = Server()
 servidor.run()
